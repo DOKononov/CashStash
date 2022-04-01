c345f3be27a53d236828a5090046265e46ea0c59
d.o.kononov <d.o.kononov@gmail.com>
2022-04-01 10:39:01 +0300

    Initial Commit

---
+++ CashStash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashStash
+//
+//  Created by Dmitry Kononov on 1.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
